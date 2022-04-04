f339af14a093617e932457499930358ceb169ac6
fanta-git <fantaji4957t@gmail.com>
2022-04-05 00:35:56 +0900

    add: マルチカーソルを指定数作る機能の追加

--- src/extension.ts
+++ src/extension.ts
@@ -6,37 +6,64 @@ export function activate(context: vscode.ExtensionContext) {
     const geneGetLineRange = (doc: vscode.TextDocument) => ({ start, end }: { start: vscode.Position, end: vscode.Position }) => doc.validateRange(
         new vscode.Selection(start.line, 0, end.line + 1, 0)
     );
+    const insertString = (index: number, text: string, word: string) => text.slice(0, index) + word + text.slice(index);
+    const range = (size: number) => [...Array(size)].map((_, i) => i);
 
-    const insertTabstops = vscode.commands.registerCommand('multicursor-utility.insertTabstops', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor === undefined) return;
-        const doc = editor.document;
-        const getLineRange = geneGetLineRange(doc);
-        const curSelections = editor.selections;
-        if (!curSelections.length) return;
+    context.subscriptions.push(
+        vscode.commands.registerCommand('multicursor-utility.convertTabstops', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor === undefined) return;
+            const doc = editor.document;
+            const getLineRange = geneGetLineRange(doc);
+            const curSelections = editor.selections;
+            if (!curSelections.length) return;
 
-        const sorted = [...curSelections].sort((a, b) => (
-            b.active.line - a.active.line || b.active.character - a.active.character
-        ));
-        const snippetRange = getLineRange({
-            start: sorted[sorted.length - 1].active,
-            end: sorted[0].active,
-        });
+            const sorted = [...curSelections].sort((a, b) => (
+                b.active.line - a.active.line || b.active.character - a.active.character
+            ));
+            const snippetRange = getLineRange({
+                start: sorted[sorted.length - 1].active,
+                end: sorted[0].active,
+            });
 
-        const text = doc.getText(snippetRange).split('\n');
-        sorted.forEach((v, i) => {
-            const line = v.active.line - sorted[sorted.length - 1].active.line;
-            const char = v.active.character;
-            const textLine = text[line];
+            const text = doc.getText(snippetRange).split('\n');
+            sorted.forEach((v, i) => {
+                const line = v.active.line - sorted[sorted.length - 1].active.line;
+                const char = v.active.character;
+                const textLine = text[line];
 
-            const newText = `${textLine.slice(0, char)}$${i && curSelections.length - i}${textLine.slice(char)}`;
-            text[line] = newText;
-        });
-        const snippet = new vscode.SnippetString(text.join('\n'));
-        editor.insertSnippet(snippet, snippetRange);
-    });
+                const newText = insertString(char, textLine, String(i && curSelections.length - i));
+                text[line] = newText;
+            });
+            const snippet = new vscode.SnippetString(text.join('\n'));
+            editor.insertSnippet(snippet, snippetRange);
+        }),
+        vscode.commands.registerCommand('multicursor-utility.insertMulticursor', async () => {
+            const editor = vscode.window.activeTextEditor;
+            if (editor === undefined) return;
+            const curSelections = editor.selections;
 
-    context.subscriptions.push(insertTabstops);
+            const input = await vscode.window.showInputBox({
+                validateInput: text => !text || /^\d*(:.+)?$/.test(text) ? null : "int (':' string)?"
+            });
+            if (!input) return;
+            const [inputNum, inputWord] = input.split(/(?<=^[^:]*):/);
+            const num = Number(inputNum);
+            const word = inputWord ?? ' ';
+
+            const newCurSelections = [] as vscode.Selection[];
+            for (const cur of curSelections) {
+                await editor.edit(edit => edit.insert(cur.active, word.repeat(num)));
+                newCurSelections.push(
+                    ...range(num).map(v => new vscode.Selection(
+                        cur.active.translate(0, word.length * (v + 1)),
+                        cur.active.translate(0, word.length * (v + 1))
+                    ))
+                );
+            }
+            editor.selections = newCurSelections;
+        }),
+    );
 }
 
 export function deactivate() {}
